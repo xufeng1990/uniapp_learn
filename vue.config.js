f9ad2abe628c8cc0cf9b520d0e2c5757b68255b0
xufeng <1192936115@qq.com>
2022-05-12 17:35:16 +0800

    commit config.js文件修改

--- vue.config.js
+++ vue.config.js
@@ -1,16 +1,10 @@
 module.exports = {
     devServer: { // 环境配置
-        host: '0.0.0.0',
-        public:'192.178.11.80:8080',
+        public:'192.178.11.81:8080',
         port: 8080,
         https: false,
         hotOnly: false,
         disableHostCheck:true,
         open: true // 配置自动启动浏览器
-     
     }
 };
-
- 
-
-
